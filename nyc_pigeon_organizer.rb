84be600b274fb891168b97965ea7fe3a6c8b4bd4
Ricky Rojas <rickyrojas19@gmail.com>
2019-04-12 15:59:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,8 @@
 def nyc_pigeon_organizer(data)
-  new_pigeons = {}
 
-  data.each do |pkeys, values|
-  values.each do |value, array|
-    array.each do |name|
+  data.each do |key, value|
+    values.each do |value, array|
+      array.each do |name|
       if value[name] == nil
     end
   end
