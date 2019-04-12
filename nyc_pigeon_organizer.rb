a157875795144a377ddd118a47d33b4eff1802ea
Ricky Rojas <rickyrojas19@gmail.com>
2019-04-12 16:02:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,11 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
-  data.each do |key, value|
-    value.each do |key2, array|
-      array.each do |name|
-        pigeon_list[name] ||= {}
-      pigeon_list[name][key] ||= []
-      pigeon_list[name][key] << key2.to_s
+    data.each do |key, value|
+      value.each do |key2, array|
+        array.each do |name|
+            pigeon_list[name] ||= {}
+          pigeon_list[name][key] ||= []
+        pigeon_list[name][key] << key2.to_s
       end
     end
   end
